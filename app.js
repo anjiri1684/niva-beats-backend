d00bc875a2c0721c5e4f86f04f617e8d1cf1a758
vincent anjiri <vincentanjiri12@gmail.com>
2024-12-08 19:10:40 +0300

    `Removed unnecessary middleware and refactored code`

--- app.js
+++ app.js
@@ -25,18 +25,15 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-// Applying middleware
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Logging middleware for debugging
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
 
-// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/beats", beatRoutes);
 app.use("/api/payments", paymentRoutes);
@@ -46,7 +43,6 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
 app.use("/checkout", paymentRoutes);
 
-// Database connection
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
